4432a3e27d1906be1809e37cec29404d0607d6fe
YiguangZhu <yzhu@stu.naperville203.org>
2021-08-23 15:02:23 -0500

    added my first program/class

---
+++ chapter 1&2/myfirst_program.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class myfirst_program here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class myfirst_program
+{
+    public static void main(String[] args}
+    {
+        System.out.println("This is a test this is only a test");
+        System.out.println("This is a test again!");
+    }
+}
