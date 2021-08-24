165b1b1103581ae564743dc719054b1e05312c38
YiguangZhu <yzhu@stu.naperville203.org>
2021-08-24 14:37:14 -0500

    error
    ; expected

--- chapter 1&2/test.java
+++ chapter 1&2/test.java
@@ -9,6 +9,6 @@ public class Test
 {
     public static void main (String[] args)
     {
-        System.out.bogus ("An emergency Broadcast");
+        System.out.println ("An emergency Broadcast")
     }
 }
