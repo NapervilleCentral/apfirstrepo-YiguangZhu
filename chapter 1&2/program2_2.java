92bb33a8af92f67253b275fb6a5194bd47a570f6
YiguangZhu <yzhu@stu.naperville203.org>
2021-08-24 15:04:22 -0500

    number displayer

---
+++ chapter 1&2/program2_2.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class program2_2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class program2_2
+{
+    public static void main (String[] args)
+    {
+        int quantity;
+        Scanner input = new Scanner (System.in);
+        System.out.println ("Input");
+        quantity = input.nextInt ();
+        System.out.println (quantity);
+    }
+}
