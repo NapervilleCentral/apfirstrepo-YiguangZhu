e4f17e5de1367d71f4a98c24c23b3d53359d8d60
YiguangZhu <yzhu@stu.naperville203.org>
2021-08-24 14:43:46 -0500

    Created 1.3

---
+++ chapter 1&2/Program1.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Program1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Program1
+{
+  public static void main (String[] args)
+  {
+      System.out.println ("Name: Yiguang Zhu");
+      System.out.println ("Birthday: 10/5/2003");
+      System.out.println ("Hobbies: playing games, lifting");
+      System.out.println ("Favorite book: Harry Potter");
+      System.out.println ("Favorite movie: Shawshank Redemption");
+}
+}
