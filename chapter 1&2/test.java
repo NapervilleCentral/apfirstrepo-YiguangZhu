74918490be91a6e927edad9649f64278e85b2004
YiguangZhu <yzhu@stu.naperville203.org>
2021-08-24 14:29:57 -0500

    changed t and fixed

--- chapter 1&2/test.java
+++ chapter 1&2/test.java
@@ -5,9 +5,10 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class test
+public class Test
 {
     public static void main (String[] args)
     {
         System.out.println ("An Emergency Broadcast");
+    }
 }
