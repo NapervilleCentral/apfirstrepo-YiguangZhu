62d8b244ae9e7dad4930f908519542524ce2fe9a
YiguangZhu <yzhu@stu.naperville203.org>
2021-09-02 14:33:53 -0500

    note

---
+++ chapter 1&2/Howtomakestudentob.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class Howtomakestudentob here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Howtomakestudentob
+{
+   Student me = new Sstudent("Kevin", "Hayes", 1234);
+}
