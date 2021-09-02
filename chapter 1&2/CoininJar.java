22600ac81cfe997bec24972bf1138a858a21359f
YiguangZhu <yzhu@stu.naperville203.org>
2021-09-02 14:40:40 -0500

    finisyhed

--- chapter 1&2/CoininJar.java
+++ chapter 1&2/CoininJar.java
@@ -6,21 +6,23 @@
  * @version (a version number or a date)
  */
 import java.util.Scanner;
+import java.text.NumberFormat;
 public class CoininJar
 {
-    public static void main(String[] args);
+    public static void main(String[] args)
     {
         Scanner coins = new Scanner(System.in);
         System.out.println("Enter numbers of quarters");
         int quarters= coins.nextInt();
-        Systen.out.println("Enter numbers of dimes");
+        System.out.println("Enter numbers of dimes");
         int dimes= coins.nextInt();
         System.out.println("Enter number of nickels");
         int nickels= coins.nextInt();
-        Systen.out.println("Enter number of pennies");
+        System.out.println("Enter number of pennies");
         int pennies= coins.nextInt();
         
         double money= 0.25*quarters+0.1*dimes+0.05*nickels+0.01*pennies;
+        NumberFormat equal = NumberFormat.getCurrencyInstance();
         System.out.println("You have: $"+money);
     }
         
